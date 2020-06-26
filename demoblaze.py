c5e8385c4ab2d4a5c331d9f9bffc07f1adfda808
Sami Sallmén <sami.sallmen@symbio.com>
2020-06-26 18:26:30 +0300

    Add disable-dev-shm-usage chrome option for headless run

--- demoblaze.py
+++ demoblaze.py
@@ -31,7 +31,7 @@ class demoblaze:
             if bool(util.strtobool(headless)):
                 chrome_options.add_argument('--headless')
                 chrome_options.add_argument('--no-sandbox')
-                #chrome_options.add_argument('--disable-dev-shm-usage')
+                chrome_options.add_argument('--disable-dev-shm-usage')
             self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.implicitly_wait(8)
         self.open_main_page()
